ecc6ca9a8da5faed671828a5a71b91ad079e1f4d
Bimarsh <bijenchhetri789@gmail.com>
2025-07-08 13:54:19 +0545

    Third change

--- base/settings.py
+++ base/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-35kr+v2j(brd=8^rwavtlc4(cr$%u=8&wi8o4w_jj=b%0(ksq=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['NoteProject.pythonanywhere.com',]
 
 
 # Application definition
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 import os
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
